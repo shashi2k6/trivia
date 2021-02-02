d559f023a7eb1fd120e9eb2b1eb7383e3e053872
shashi <shashi2k6@gmail.com>
2021-02-02 18:53:29 -0500

    Initial commit

---
+++ src/test/java/com/gal/trivia/TriviaApiTest.java
@@ -0,0 +1,17 @@
+package com.gal.trivia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase
+@SpringBootTest
+public class TriviaApiTest {
+
+    @Test
+    public void test_method() {
+        
+    }
+}
